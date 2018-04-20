137a4adbfb3d09776640da568930632219ea94bf
Jae-Yong Choi <jayong93@gmail.com>
2018-04-20 15:15:15 +0900

    docker-compose 설치

--- custom_install/docker.sh
+++ custom_install/docker.sh
@@ -14,3 +14,7 @@ sudo add-apt-repository \
 # install docker
 apt update
 apt install -y docker-ce
+
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
