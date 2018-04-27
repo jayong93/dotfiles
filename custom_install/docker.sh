e58ba5cc63bc4c1fdff5121883fddfeb9228b175
Choi Jae-Yong <jayong93@gmail.com>
2018-04-28 01:00:42 +0900

    설치 과정 더 자동화

--- custom_install/docker.sh
+++ custom_install/docker.sh
@@ -1,6 +1,6 @@
 # add docker apt repository
-apt update
-apt install -y \
+sudo apt update
+sudo apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -12,9 +12,14 @@ sudo add-apt-repository \
    stable"
 
 # install docker
-apt update
-apt install -y docker-ce
+sudo apt update
+sudo apt install -y docker-ce
 
 # install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# make env file for docker-compose
+echo "QBT_UID=$(id -u qbtuser)\nQBT_GID=$(id -g qbtuser)" > ${DOTFILES_DIR}/docker/.env
+
+sudo docker-compose -d up
