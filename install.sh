4651916661d2234d43d1172c768b28aa794afb73
Choi Jae-Yong <jayong93@gmail.com>
2018-05-24 16:11:03 +0900

    ftp 유저 파일을 입력으로 받아서 설치시 자동으로 추가

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-CURRENT_SCRIPT=$0
+if [ ! -z "${1}" ] && [ -f "${1}" ]; then
+    FTP_USER_DATA="${1}"
+else
+    echo "you should give a file which has ftp user name and passwd in each line of itself."
+    exit 1
+fi
 export DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # installation by apt (include zsh)
@@ -18,13 +23,13 @@ sudo groupadd qbtuser -g 9999
 sudo useradd qbtuser -g qbtuser -s /usr/sbin/nologin
 
 # custom installation (include oh-my-zsh)
-for CUSTOM_APP in `find "$DOTFILES_DIR/custom_install" -type f`; do
-    APP_NAME=$(basename "$CUSTOM_APP")
+for CUSTOM_APP in `find "${DOTFILES_DIR}/custom_install" -type f`; do
+    APP_NAME=$(basename "${CUSTOM_APP}")
     APP_NAME=${APP_NAME%.*}
     echo "[INFO] Install $APP_NAME"
-    (/bin/bash "$CUSTOM_APP" &&
-        echo "[INFO] $APP_NAME has been installed") ||
-        echo "[INFO] $APP_NAME has been failed installing"
+    (/bin/bash "${CUSTOM_APP}" &&
+        echo "[INFO] ${APP_NAME} has been installed") ||
+        echo "[INFO] ${APP_NAME} has been failed installing"
 done
 echo "==========================="
 echo "[INFO] Custom installation is done"
@@ -32,14 +37,14 @@ echo "==========================="
 
 # link runcom files
 echo "source ${DOTFILES_DIR}/rc/.zprofile" >> ~/.zshrc
-ln -sfv "$DOTFILES_DIR/rc/.vimrc" ~
+ln -sfv "${DOTFILES_DIR}/rc/.vimrc" ~
 
 # do post-process
-POST_SCRIPT="$DOTFILES_DIR/postprocess.sh"
+POST_SCRIPT="${DOTFILES_DIR}/postprocess.sh"
 echo "==========================="
 echo "[INFO] Do Postprocess"
 echo "==========================="
-/bin/bash "$POST_SCRIPT"
+/bin/bash "${POST_SCRIPT}" "${FTP_USER_DATA}"
 echo "==========================="
 echo "[INFO] Done!"
 echo "==========================="
