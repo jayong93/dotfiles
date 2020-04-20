25d924c772dfa793f199839950416f9d2fe9b239
Jaeyong Choi <jayong93@gmail.com>
2020-04-21 01:09:19 +0900

    don't install docker

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-if [ ! -z "${1}" ] && [ -f "${1}" ]; then
-    FTP_USER_DATA="${1}"
-else
-    echo "you should give a file which has ftp user name and passwd in each line of itself."
-    exit 1
-fi
 export DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # installation by apt (include zsh)
@@ -17,10 +10,6 @@ xargs -r -a "${DOTFILES_DIR}/install_list" sudo apt install -y --fix-missing
 echo "==========================="
 echo "[INFO] Installation by apt is done"
 echo "==========================="
-
-# add user and group for qbittorrent and the files which is downloaded via it
-sudo groupadd qbtuser -g 9999
-sudo useradd qbtuser -g qbtuser -s /usr/sbin/nologin
 
 # custom installation (include oh-my-zsh)
 for CUSTOM_APP in `find "${DOTFILES_DIR}/custom_install" -type f`; do
--- postprocess.sh
+++ postprocess.sh
@@ -1,16 +1,2 @@
 # install vim plugins
 vim -c PlugInstall -c qa
-
-# add ftp user account to pure-ftpd
-USER_FILE="${1}"
-USER_FILE_LINES=$(wc ${USER_FILE} | awk '{print $1}')
-
-if [ $USER_FILE_LINES -ne 2 ]; then
-    echo "user file should has exactly two lines." 1>&2
-    return 1
-fi
-
-USER_NAME="$(head -1 ${USER_FILE})"
-USER_PW="$(tail -1 ${USER_FILE})"
-docker exec -it ftp bash -c "useradd qbtuser -u $(id -u qbtuser)"
-docker exec -it ftp bash -c "printf \"%s\n\" ${USER_PW} ${USER_PW} | pure-pw useradd ${USER_NAME} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u qbtuser -g $(id -g qbtuser) -d /home/ftpusers"
