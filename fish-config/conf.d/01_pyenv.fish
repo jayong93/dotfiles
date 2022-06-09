bee1181193a2522799f1174567ad3468686170cc
Jaeyong-Choi <jayong93@linecorp.com>
2022-06-09 09:04:58 +0900

    Add pyenv setting for virtualenv

--- fish-config/conf.d/01_pyenv.fish
+++ fish-config/conf.d/01_pyenv.fish
@@ -1,4 +1,5 @@
 if which pyenv > /dev/null
     status is-login; and pyenv init --path | source
     status is-interactive; and pyenv init - | source
+    status is-interactive; and pyenv virtualenv-init - | source
 end
