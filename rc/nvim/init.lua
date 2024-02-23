8314df717f5fbd28afc90fbb4e54ac91aebbe88b
Choi Jaeyong <jayong93@gmail.com>
2024-02-23 16:54:58 +0900

    Use Astronvim instead of LazyVim

--- rc/nvim/init.lua
+++ rc/nvim/init.lua
@@ -1,8 +1,23 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
 
-local function reload(m)
-  package.loaded[m] = nil
-  return require(m)
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
 end
 
-reload("config.lazy")
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+      vim.log.levels.ERROR
+    )
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
