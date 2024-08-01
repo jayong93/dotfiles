e68bb87c3d388ba8b715df405974e410d6d9843d
Jaeyong Choi <jayong93@linecorp.com>
2024-08-01 23:45:37 +0900

    Configure nvim plugins

--- rc/nvim/lua/plugins/diffdirs.lua
+++ rc/nvim/lua/plugins/diffdirs.lua
@@ -5,10 +5,16 @@ return {
     opts = {},
     cmd = {"DiffDirs"},
     config = function (_, opts)
-      require("diffdirs").setup(opts)
-      vim.keymap.set('n', '[q', '<Plug>PrevDiff', {silent=true, noremap=true})
-      vim.keymap.set('n', ']q', '<Plug>NextDiff', {silent=true, noremap=true})
-    end
+      local diffdirs = require("diffdirs")
+      diffdirs.setup(opts)
+      vim.keymap.set('n', '[q', '<Plug>PrevDiff', {silent=true, noremap=true, desc="Previous diff tab"})
+      vim.keymap.set('n', ']q', '<Plug>NextDiff', {silent=true, noremap=true, desc="Next diff tab"})
+      vim.keymap.set('n', '<Leader>fd', function()
+        require('fzf-lua').fzf_exec(diffdirs.diff_files(),
+          {actions = {['default'] = function(selected) diffdirs.jump_diff_tab(selected[1]) end}})
+      end, {silent=true, noremap=true, desc="Find diff tabs"})
+    end,
+    dependencies = {"ibhagwan/fzf-lua"}
   },
   {
     "julienvincent/hunk.nvim",
--- rc/nvim/lua/plugins/snipe_nvim.lua
+++ rc/nvim/lua/plugins/snipe_nvim.lua
@@ -1,8 +1,8 @@
+---@type LazySpec
 return {
   "leath-dub/snipe.nvim",
-  config = function()
-    local snipe = require("snipe")
-    snipe.setup()
-    vim.keymap.set("n", "<Leader>fb", snipe.create_buffer_menu_toggler())
-  end
+  opts = {},
+  keys = {
+    {"<Leader>fb", function() require("snipe").create_buffer_menu_toggler() end, desc="Find buffer"}
+  }
 }
