4875853cb2c87921882289d12d5bc40ff95d6bed
Jaeyong Choi <jayong93@linecorp.com>
2025-03-10 10:16:36 +0900

    Config markview.nvim plugin

--- rc/nvim/lua/plugins/markview-nvim.lua
+++ rc/nvim/lua/plugins/markview-nvim.lua
@@ -4,6 +4,13 @@ return {
   lazy = false,      -- Recommended
   -- ft = "markdown" -- If you decide to lazy-load anyway
 
+  opts = {
+    markdown = {
+      list_items = {
+        shift_width = 2
+      }
+    }
+  },
   dependencies = {
     -- You will not need this if you installed the
     -- parsers manually
