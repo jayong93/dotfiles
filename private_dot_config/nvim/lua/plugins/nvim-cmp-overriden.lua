d110b20dcde86303172cc1d5c3d51a4e6382da7c
Jaeyong Choi <jayong93@gmail.com>
2026-02-19 21:37:00 +0900

    Manage nvim config in chezmoi no more
    It's now managed [here](https://gitlab.com/jayong93/my-nvim-config#).

--- private_dot_config/nvim/lua/plugins/nvim-cmp-overriden.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-  -- "hrsh7th/nvim-cmp",
-  -- opts = function (_, opts)
-  --   local cmp = require("cmp")
-  --   opts.mapping["<CR>"] = cmp.mapping.confirm({select = false})
-  -- end
-}
--- private_dot_config/nvim/lua/plugins/snipe_nvim.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-return {
-  "leath-dub/snipe.nvim",
-  -- opts = {},
-  -- keys = {
-  --   {"<Leader>fb", function() require("snipe").open_buffer_menu() end, desc="Find buffer"}
-  -- }
-  enabled = false
-}
